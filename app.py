06e37527ba8d9605477848e19aa6d04748d67539
Kevin <kkemp3@wisc.edu>
2020-02-19 21:34:08 -0600

    back end recognizing post requests

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/logininfo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/loginfo'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 
 class LoginInfo(db.Model):
     __tablename__ = "logins"
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(), unique=True)
-    password = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(40))
 
     def __init__(username, password):
         this.username = username
         this.password = password
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
@@ -21,17 +27,18 @@ def index():
     return render_template('index.html')
 
 # Save e-mail to database and send to success page
-@app.route('/insert', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def insert():
     username = None
     if request.method == 'POST':
-        username = request.form['username']
-        # Check that email does not already exist (not a great query, but works)
-        if not db.session.query(User).filter(User.username == username).count():
-            username = User(username)
-            db.session.add(usernmae)
-            db.session.commit()
-            return render_template('success.html')
+        content = request.json
+        print(content)
+        #print('username' + username)
+        # if not db.session.query(User).filter(User.username == username).count():
+        #     username = User(username)
+        #     db.session.add(usernmae)
+        #     db.session.commit()
+        return render_template('success.html')
     return render_template('index.html')
 
 
